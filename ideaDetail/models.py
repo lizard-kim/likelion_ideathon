47e01f17f6bd212708171e6e9599364f4b3f06f2
choikwangil <choikwangil0@naver.com>
2020-05-01 17:47:35 +0900

    d

--- ideaDetail/models.py
+++ ideaDetail/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Comment(models.Model):
+    text = models.CharField(max_length = 200)
+    create_data = models.DateTimeField(default = timezone.now) # 생성날짜
+
+    def __str__(self):
+        return str(self.text)
--- ideaDetail/views.py
+++ ideaDetail/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render, redirect
+from ideaDetail.models import Comment
+from django.utils import timezone
 
 def detail(request):
-    return render(request, 'detail.html')
+    
+    if request.method == 'POST':
+        comment = Comment()
+        comment.text = request.POST['comment']
+        comment.create_data = timezone.datetime.now()
+        comment.save()
+
+        return redirect('detail')
+
+    else:
+        comment_list = Comment.objects.all()
+
+    return render(request, 'detail.html',{'comment_list' : comment_list})
