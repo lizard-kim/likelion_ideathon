c74633efe149c9e006332748e0162a951ee1184f
choikwangil <choikwangil0@naver.com>
2020-05-02 00:11:07 +0900

    d

--- ideaDetail/models.py
+++ ideaDetail/models.py
@@ -9,3 +9,11 @@ class Comment(models.Model):
 
     def __str__(self):
         return str(self.text)
+
+class AddComment(models.Model):
+    comment_pk = models.ForeignKey (Comment, on_delete=models.CASCADE, blank=True, null = True)
+    text = models.CharField(max_length = 200)
+    create_data = models.DateTimeField(default = timezone.now) # 생성날짜
+
+    def __str__(self):
+        return str(self.text)
--- ideaDetail/views.py
+++ ideaDetail/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render, redirect
-from ideaDetail.models import Comment
+from ideaDetail.models import Comment, AddComment
 from django.utils import timezone
 
 def detail(request):
     
     if request.method == 'POST':
-        comment = Comment()
-        comment.text = request.POST['comment']
-        comment.create_data = timezone.datetime.now()
-        comment.save()
+        if request.POST.get == 'comment':
+            comment = Comment()
+            comment.text = request.POST.get('comment', '')
+            comment.create_data = timezone.datetime.now()
+            comment.save()
+        else :
+            addcomment = AddComment()
+            addcomment.text = request.POST.get('addcomment', '')
+            addcomment.create_data = timezone.datetime.now()
+            addcomment.save()
 
         return redirect('detail')
 
     else:
         comment_list = Comment.objects.all()
+        addcomment_list = AddComment.objects.all()
 
-    return render(request, 'detail.html',{'comment_list' : comment_list})
+    return render(request, 'detail.html',{
+        'comment_list' : comment_list,
+        'addcomment_list' : addcomment_list
+        })
