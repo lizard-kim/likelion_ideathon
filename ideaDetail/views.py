54669e0b213b385a0aa91836fac7d642da5a9a66
choikwangil <choikwangil0@naver.com>
2020-05-02 10:32:08 +0900

    d

--- ideaDetail/views.py
+++ ideaDetail/views.py
@@ -5,24 +5,29 @@ from django.utils import timezone
 def detail(request):
     
     if request.method == 'POST':
-        if request.POST.get == 'comment':
+
+        comment = request.POST.get('comment', 0)
+        addcomment = request.POST.get('addcomment', 0)
+
+        if 'comment' in request.POST:
             comment = Comment()
-            comment.text = request.POST.get('comment', '')
+            comment.text = request.POST['comment']
             comment.create_data = timezone.datetime.now()
             comment.save()
-        else :
+
+        elif 'addcomment' in request.POST:
             addcomment = AddComment()
-            addcomment.text = request.POST.get('addcomment', '')
+            addcomment.text = request.POST['addcomment']
             addcomment.create_data = timezone.datetime.now()
             addcomment.save()
-
+        
         return redirect('detail')
 
     else:
         comment_list = Comment.objects.all()
         addcomment_list = AddComment.objects.all()
 
-    return render(request, 'detail.html',{
-        'comment_list' : comment_list,
-        'addcomment_list' : addcomment_list
+        return render(request, 'detail.html',{
+            'comment_list' : comment_list,
+            'addcomment_list' : addcomment_list
         })
