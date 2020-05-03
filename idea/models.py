7d7b6209fdd093831393c986a2b546ace05f02d6
Hyo Gyeong <cdkrcd8@gmail.com>
2020-05-04 06:15:04 +0900

    model edit

--- idea/models.py
+++ idea/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Idea (models.Model):
+    image = models.ImageField()
+    title = models.CharField(max_length = 100)
+    sub_title = models.CharField(max_length = 100)
+    pub_date = models.DateTimeField(default=datetime.now,blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-id']
