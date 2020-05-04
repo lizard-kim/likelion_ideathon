ea1ca70c4f429490885e9d6f14402fa3f8cb3e51
choikwangil <choikwangil0@naver.com>
2020-05-04 20:37:31 +0900

    모델 수정

--- idea/admin.py
+++ idea/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Idea, Image_storage
+from .models import Idea, Image_storage, Comment,AddComment
 # Register your models here.
 
 admin.site.register(Idea)
 admin.site.register(Image_storage)
+admin.site.register(Comment)
+admin.site.register(AddComment)
--- idea/models.py
+++ idea/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from signIn.models import Profile
 from django.contrib.auth.models import User
-
-# Create your models here.
+from django.utils import timezone
 
 class Idea(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -11,7 +9,8 @@ class Idea(models.Model):
     idea_image = models.ImageField(upload_to="", null = True, blank = True)
     idea_description = models.TextField(max_length=500)
     idea_hashtag = models.TextField(max_length=100)
-    idea_likecount = models.IntegerField()
+    idea_likecount = models.IntegerField(null = True, blank = True)
+    # idea_create_data = models.DateTimeField(default = timezone.now, null = True, blank = True) # 생성날짜
 
     def __str__(self):
         return str(self.idea_title)
@@ -22,3 +21,19 @@ class Image_storage(models.Model):
 
     def __str__(self):
         return str(self.idea_title)
+
+class Comment(models.Model):
+    # user = models.ManyToManyField(User)
+    text = models.CharField(max_length = 200)
+    create_data = models.DateTimeField(default = timezone.now) # 생성날짜
+    
+    def __str__(self):
+        return str(self.text)
+
+class AddComment(models.Model):
+    comments_pk = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    text = models.CharField(max_length = 200, blank = True, null = True)
+    create_data = models.DateTimeField(default = timezone.now) # 생성날짜
+
+    def __str__(self):
+        return str(self.text)
