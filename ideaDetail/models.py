2130c390698caecd1d4e5e13cc5d195844e22edb
choikwangil <choikwangil0@naver.com>
2020-05-04 16:25:36 +0900

    create Comment model

--- ideaDetail/models.py
+++ ideaDetail/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_pk = models.AutoField(primary_key = True)
+    text = models.CharField(max_length = 200)
+    create_data = models.DateTimeField(default = timezone.now) # 생성날짜
+    
+    def __str__(self):
+        return str(self.text)
+
+class AddComment(models.Model):
+    comment_pk = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length = 200, blank = True, null = True)
+    create_data = models.DateTimeField(default = timezone.now) # 생성날짜
+
+    def __str__(self):
+        return str(self.text)
