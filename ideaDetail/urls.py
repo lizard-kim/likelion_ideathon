09d2f968008634ae942812f2ff98669a5c25a200
Hyo Gyeong <cdkrcd8@gmail.com>
2020-05-07 03:51:04 +0900

    detail edit & delete

--- ideaDetail/urls.py
+++ ideaDetail/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('<int:detail_id>', views.detail, name = "detail"),
     path('<int:detail_id>/delete', views.delete, name = "delete"),
+    path('<int:detail_id>/edit', views.edit, name = "edit"),
+    
 ]
--- ideaDetail/views.py
+++ ideaDetail/views.py
@@ -4,10 +4,24 @@ from idea.models import Idea
 def detail(request, detail_id):
     idea_detail = get_object_or_404(Idea, pk = detail_id)
     full_hash_tag = idea_detail.idea_hashtag
-    hash_tag = full_hash_tag.split()
+    hash_tag = full_hash_tag.replace(',','').split()
     return render(request, 'detail.html', {'detail':idea_detail, 'hasg_tag':hash_tag})
 
 def delete(request, detail_id):
     idea_detail = get_object_or_404(Idea, pk = detail_id)
     idea_detail.delete()
     return redirect('idea')
+
+def edit(request, detail_id):
+    idea_detail = get_object_or_404(Idea, pk = detail_id)
+
+    if request.method == 'POST':
+        idea_detail.idea_title = request.POST['IdeaName']
+        idea_detail.idea_subtitle = request.POST['IdeaSubtitle']
+        idea_detail.idea_description = request.POST['IdeaContent']
+        #idea_detail.idea_image
+        idea_detail.idea_hashtag = request.POST['IdeaHashTag']
+        idea_detail.save()
+        return redirect('/detail/' + str(detail_id))
+    else:
+        return render(request, 'submit.html', {'idea_detail':idea_detail})
