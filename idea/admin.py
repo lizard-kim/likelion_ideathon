7ee59df5f005fbde8e25e7a59ec86bbcf036e289
choikwangil <choikwangil0@naver.com>
2020-05-08 17:40:53 +0900

    d

--- idea/admin.py
+++ idea/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Idea1, Idea_image_storage1, Idea_Comments1, Idea_AddComments1
 # Register your models here.
+
+admin.site.register(Idea1)
+admin.site.register(Idea_image_storage1)
+admin.site.register(Idea_Comments1)
+admin.site.register(Idea_AddComments1)
