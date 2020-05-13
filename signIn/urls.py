27dc4abd1a2c6725781e65816265c860a7ff4213
JeeHyungPark <jhp0227@naver.com>
2020-05-14 04:01:35 +0900

    [#49] 로그인 기능

--- signIn/urls.py
+++ signIn/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import signin
+from .views import signin, logout
 
 urlpatterns = [
     path('', signin, name="signin"),
+    path('logout/', logout, name="logout"),
 ]
--- signIn/views.py
+++ signIn/views.py
@@ -4,5 +4,20 @@ from django.contrib import auth
 from .models import *
 
 def signin(request):
-    
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(request, email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('idea')
+        else:
+            return render(request, 'signin.html', {'alert': '이메일 또는 비밀번호를 다시 한 번 확인해주세요!'})
+    else:
+        return render(request, 'signin.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('')
     return render(request, 'signin.html')
