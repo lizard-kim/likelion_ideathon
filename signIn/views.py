d42ec245820efea4039c1fcc64b5a7a6571415b8
PeterPPark <petepp@naver.com>
2020-05-17 20:44:02 +0900

    test

--- signIn/views.py
+++ signIn/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from .models import *
 
 def signin(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(request, email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('idea')
+        else:
+            return render(request, 'signin.html', {'alert': '이메일 또는 비밀번호를 다시 한 번 확인해주세요!', 'user':user})
+    else:
+        return render(request, 'signin.html')
+
+# @ brief: 
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('')
     return render(request, 'signin.html')
