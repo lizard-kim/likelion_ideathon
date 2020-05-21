85db79e67c1fbea9cb7c583fee086a02a4b61ccd
lizard-kim <rlaxodud1122@gmail.com>
2020-05-21 18:43:09 +0900

    for save

--- submit/views.py
+++ submit/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from idea.model import *
 
 def submit(request):
-    return render(request, 'submit.html')
+    if request.method == 'POST':
+        title = request.POST['title']
+        contents = request.POST['contents']
+
+        newidea = Idea.objects.create(
+            title = title,
+            contents = contents,
+        )
+        return redirect('../')
+
+    else:
+        return render(request, 'submit.html')
