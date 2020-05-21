2db770322d8270ff4969a1b1c869c97b37ae1353
lizard-kim <rlaxodud1122@gmail.com>
2020-05-21 23:00:40 +0900

    for submit page

--- submit/views.py
+++ submit/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
-from idea.model import *
+'''
+import os, sys
+from .model import *
+'''
 
 def submit(request):
+    '''
     if request.method == 'POST':
         title = request.POST['title']
         contents = request.POST['contents']
+        image = request.FILE.get('image')
 
         newidea = Idea.objects.create(
             title = title,
             contents = contents,
         )
+
+        newimage = Idea_image_storage.objects.create(
+            idea = newidea,
+            image = image
+        )
+
         return redirect('../')
 
     else:
-        return render(request, 'submit.html')
+    '''
+    return render(request, 'submit.html')
