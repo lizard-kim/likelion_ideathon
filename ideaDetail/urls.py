cc4b312e4ad605f123ce0aef0f4e10d15dad7eff
choikwangil <choikwangil0@naver.com>
2020-05-25 03:26:47 +0900

    d

--- ideaDetail/urls.py
+++ ideaDetail/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('<int:detail_id>/<int:comment_id>/comment_edit', views.comment_edit, name = "comment_edit"),
+    path('<int:detail_id>/<int:comment_id>/comment_delete', views.comment_delete, name = "comment_delete"),
     path('<int:detail_id>', views.detail, name = "detail"),
     path('<int:detail_id>/delete', views.delete, name = "delete"),
     path('<int:detail_id>/edit', views.edit, name = "edit"),
-    path('<int:detail_id>/<int:comment_id>/comment_edit', views.comment_edit, name = "comment_edit"),
-    path('<int:detail_id>/<int:comment_id>/comment_delete', views.comment_delete, name = "comment_delete"),
 ]
