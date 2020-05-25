47320bfb38da2ddfe087e96b5d10aec0e6a78cd1
lizard-kim <rlaxodud1122@gmail.com>
2020-05-25 20:34:41 +0900

    [#93]pwdedit clear

--- mypage/views.py
+++ mypage/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from accounts.models import Profile, Idea_Cart
+from django.contrib import auth
 from idea.models import Idea, Idea_Comments, Idea_AddComments
+from django.contrib.auth.hashers import check_password
 from accounts.forms import *
 
 def mypage(request):
@@ -12,12 +14,17 @@ def mypage(request):
         comment_all = comment + add_comment
         cart = Idea_Cart.objects.filter(user = request.user)
         cart_all = Idea_Cart.objects.all().exclude(user = request.user).count()
+
+        form = UserChangeForm()
+
         return render(request, 'mypage.html', 
             {'profile' : profile, 
             'myidea' : myidea, 
             'comment_all' : comment_all, 
             'cart_all' : cart_all,
-            'cart' : cart}
+            'cart' : cart,
+            'form' : form
+            }
         )
     else:
         
@@ -25,11 +32,15 @@ def mypage(request):
 
 def edit(request):
     if request.user.is_authenticated == True and request.method == 'POST':
-        form = UserChangeForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.save()
-            return redirect('./')
+        password = request.POST['password']
+        new_password = request.POST['new_password']
+        password_confirm = request.POST['password_confirm']
+        user = request.user
+        if check_password(password,user.password) and new_password == password_confirm:
+            user.set_password(new_password)
+            user.save()
+            auth.login(request, user)
+            return redirect('../')
         return render(request, 'mypageedit.html',{
             'form' : form
         })
