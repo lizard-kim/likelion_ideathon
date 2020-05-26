a7ce4f9a4b4e5926e2ec9b3842770f53863ac28c
lizard-kim <rlaxodud1122@gmail.com>
2020-05-26 18:55:24 +0900

    오타 수정, 개수 정상 출력

--- idea/views.py
+++ idea/views.py
@@ -7,9 +7,10 @@ from django.core.cache import cache
 
 def idea(request):
     ideas = Idea.objects.all().order_by('?')
+    count = ideas.count()
     profile = Profile.objects.all()
     paginator = Paginator(ideas,  12)
     page = request.GET.get('page')
     posts = paginator.get_page(page) 
     page_range = 6
-    return render(request, 'idea.html', {'ideas':ideas, 'posts':posts, 'profile' : profile})
+    return render(request, 'idea.html', {'ideas':ideas, 'posts':posts, 'profile' : profile, 'count' : count})
