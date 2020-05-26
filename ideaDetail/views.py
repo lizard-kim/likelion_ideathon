3c1121ae549a6e803b832850415be692fea65bcc
lizard-kim <rlaxodud1122@gmail.com>
2020-05-26 18:59:35 +0900

    detail profile bugfix

--- ideaDetail/views.py
+++ ideaDetail/views.py
@@ -233,6 +233,8 @@ def who(request, detail_id):
     idea_user = idea_detail.user
 
     if request.user.is_authenticated :
-            current_user = request.user
+        current_user = request.user
+        return render(request, "who.html", {'idea_user':idea_user ,'current_user':current_user})
+    else:
+        return render(request, 'signin.html')
 
-    return render(request, "who.html", {'idea_user':idea_user ,'current_user':current_user})
