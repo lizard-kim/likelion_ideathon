db56230506b2216e2bb563309d114f4ab37fb2be
PeterPPark <petepp@naver.com>
2020-05-30 02:28:24 +0900

    Test

--- idea/models.py
+++ idea/models.py
@@ -9,7 +9,7 @@ class Idea(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
     idea_title = models.CharField(max_length=20, null=True, blank=True)
-    idea_subtitle = models.TextField(max_length=100, null=True, blank=True)
+    idea_subtitle = models.TextField(max_length=40, null=True, blank=True)
     idea_image = models.ImageField(
         upload_to="idea/%Y%m%d", null=True, blank=True)
     idea_description = models.TextField(max_length=500, null=True, blank=True)
