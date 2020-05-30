40dc41ac0cb68209c5c6a6dc6de2362b1e5ee5b8
PeterPPark <petepp@naver.com>
2020-05-30 20:13:03 +0900

    [#157]please final

--- mutsa_bulk.py
+++ mutsa_bulk.py
@@ -1,3 +1,4 @@
+from accounts.models import Profile
 import os
 import django
 import csv
@@ -5,7 +6,6 @@ import csv
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ideathon.settings')
 django.setup()
 
-from accounts.models import Profile
 
 f = open('../멋사 - Sheet1.csv', 'r', encoding="utf8")
 mutsa = []
@@ -19,6 +19,6 @@ f.close()
 
 instances = []
 for (email, user_name, user_school, password) in mutsa:
-    instances.append(Profile(email=email, user_name=user_name, user_school=user_school, password=password))
+    instances.append(Profile(email=email, user_name=user_name, user_school=user_school, password=password  user_about="자기소개를 변경해주세요.",  user_contact="연락처를 적어주세요."))
 
 Profile.objects.bulk_create(instances)
